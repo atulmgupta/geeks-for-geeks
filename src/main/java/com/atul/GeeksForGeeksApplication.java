d0db2b2a71ed21ffe7e16ccf7ff3c2a9e1882f15
atul <iamatulmgupta@gmail.com>
2020-08-29 12:26:47 -0700

    Added

---
+++ src/main/java/com/atul/GeeksForGeeksApplication.java
@@ -0,0 +1,13 @@
+package com.atul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeeksForGeeksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeeksForGeeksApplication.class, args);
+	}
+
+}
